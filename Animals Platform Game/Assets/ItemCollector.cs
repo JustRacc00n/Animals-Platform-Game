0332e3c3d7fd6d58f3a80b1ee6b0c157bb600bbc
For3stu <70603167+For3stu@users.noreply.github.com>
2023-01-14 16:44:01 +0100

    Collectibles

---
+++ Animals Platform Game/Assets/ItemCollector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int Apple = 0;
+
+    [SerializeField] private Text ApplesText;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Apple"))
+        {
+            Destroy(collision.gameObject);
+            Apple++;
+            ApplesText.text = "Apples: " + Apple;
+        }
+    }
+}
