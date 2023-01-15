fe270a7d4289db9d32e6dfd125fbddc3c2d61703
JustRacc00n <barteklew123@gmail.com>
2023-01-15 17:56:53 +0100

    added background music

---
+++ Animals Platform Game/Assets/DontDestroy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("Music");
+        if (obj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
